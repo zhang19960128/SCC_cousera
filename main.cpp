287c847c88d781fb514158c7b24e6235a820c357
zjh19960128 <zjh199601283492@gmail.com>
2019-01-01 17:07:20 -0500

    good

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <list>
 #include <vector>
 /*status 0(white) represent not visited, 1(gray) represent visited but not finished DFS search tree not stopped,2(Black) represents DFS search tree stoped */
-void DFS_v(std::vector<std::list<int> > &graph,std::vector<int>& status,std::vector<int>& f,int node,int times){
+/*f map the original node ----> magic order*/
+/*g map the magic order -----> original node*/
+void DFS_v_mark(std::vector<std::list<int> > &graph,std::vector<int>& status,std::vector<int>& f,std::vector<int>& g,int node,int& times){
 	status[node]=1;
 	for(std::list<int>::iterator a=graph[node].begin();a!=graph[node].end();a++){
-		std::cout<<"I am visiting: "<<*a<<" "<<graph[*a].size()<<std::endl;
 		if(status[*a]==0){
-	 		DFS_v(graph,status,f,*a,times);
+	 		DFS_v_mark(graph,status,f,g,*a,times);
 		}
 	}
 	status[node]=2;
 	f[node]=times;
+        g[times]=node;
 	times=times+1;
 }
-void DFS(std::vector<std::list<int> >&graph,std::vector<int>& status,std::vector<int>& f){
+void DFS(std::vector<std::list<int> >& graph,std::vector<int>& status,std::vector<int>& f,std::vector<int>& g){
 	int times=1;
 	size_t size=status.size();
 	for(size_t i=1;i<size;i++){
 		status[i]=0;
+                f[i]=0;
+                g[i]=0;
 	}
 	for(size_t i=1;i<size;i++){
-		if(f[i]==0){
-			std::cout<<i<<std::endl;
-			DFS_v(graph,status,f,i,times);
+		if(status[i]==0){
+			DFS_v_mark(graph,status,f,g,i,times);
 		}
 	}
+}
+void DFS_v(std::vector<std::list<int> > &graph,std::list<int> &vlist,std::vector<int>& status,int node){
+      status[node]=1;
+      for(std::list<int>::iterator a=graph[node].begin();a!=graph[node].end();a++){
+         if(status[*a]==0){
+            DFS_v(graph,vlist,status,*a);
+            vlist.push_back(*a);
+         }
+      }
+}
+void SCC(std::vector<std::list<int> >& graph,std::vector<std::list<int> >& gcc_graph,std::vector<int>& f,std::vector<int>& g){
+      size_t size=g.size();
+      std::vector<int> status(size,0);
+      for(size_t i=size-1;i>=1;i++){
+         if(status[i]==0){
+
+         }
+      }
 }
 int main(){
 	std::fstream fs;
@@ -56,9 +78,7 @@ int main(){
 	std::cout<<"reverse succeed!"<<std::endl;
 	std::vector<int> status(i,0);
 	std::vector<int> f(i,0);
-	DFS(graph,status,f);
-	//std::sort(f.begin(),f.end());
-	for(i=0;i<f.size();i++){
-	//	std::cout<<f[i]<<std::endl;
-	}
-}	
+        std::vector<int> g(i,0);
+	DFS(graph_re,status,f,g);//compute the magic order.
+
+}
