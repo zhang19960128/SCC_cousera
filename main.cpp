a42d0464da175b0140d17738db5c219e12276dd6
Jiahao Zhang <jiahaozhang@Jiahaos-MacBook-Pro.local>
2019-01-01 11:52:14 -0500

    chagne

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+/*status 0(white) represent not visited, 1(gray) represent visited but not finished DFS search tree not stopped,2(Black) represents DFS search tree stoped */
+void DFS_v(std::vector<std::list<int> > &graph,std::vector<int>& status,std::vector<int>& f,int node,int &times){
+	status[node]=1;
+	for(std::list<int>::iterator i=graph[node].begin();i!=graph[node].end();i++){
+		if(status[*i]==0){
+			DFS_v(graph,status,*i);
+		}
+	}
+	status[node]=2;
+	f[node]=times;
+	times=times+1;
+}
+void DFS(std::vector<std::list<int> >&graph,std::vector<int>& status,std::vector<int>& f){
+	int times=1;
+	size_t size=status.size();
+	for(size_t i=1;i<size;i++){
+		status[i]=0;
+	}
+	for(size_t i=1;i<size;i++){
+		if(f[i]==0){
+			DFS_v(graph,status,f,i,times);
+		}
+	}
+}
+int main(){
+	std::fstream fs;
+	fs.open("scc.txt",std::fstream::in);
+	std::vector<std::list<int> > graph;
+	graph.push_back(std::list<int>(0,0));
+	int one,two;
+	one=1;
+	while(fs>>one){
+		fs>>two;
+		if(one>=graph.size()){
+			for(size_t i=graph.size();i<=one;i++){
+				graph.push_back(std::list<int>(0,0));
+			}
+			graph[one].push_back(two);
+		}
+		else{
+			graph[one].push_back(two);
+		}
+	}
+	std::vector<std::list<int> > graph_re(graph.size(),std::list<int>(0,0));
+	int i=graph.size();
+	for(size_t j=0;j<i;j++)
+		for(std::list<int>::iterator k=graph[j].begin();k!=graph[j].end();k++){
+			graph_re[*k].push_back(j);
+		}
+	std::cout<<"reverse succeed!"<<std::endl;
+}	
